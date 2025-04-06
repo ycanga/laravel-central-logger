36958b3e88bb25c266eafd9880c8223999809ee7
Ycanga <canayunus@gmail.com>
2025-04-06 14:43:08 +0300

    Method updated.

--- src/LoggerHandler.php
+++ src/LoggerHandler.php
@@ -4,6 +4,7 @@ namespace ycanga\CentralLogger;
 
 use Monolog\Handler\AbstractProcessingHandler;
 use Monolog\Logger;
+use Monolog\LogRecord;
 use Illuminate\Support\Facades\Http;
 
 class LoggerHandler extends AbstractProcessingHandler
@@ -18,17 +19,17 @@ class LoggerHandler extends AbstractProcessingHandler
         $this->apiKey = config('central-logger.api_key');
     }
 
-    protected function write(array $record): void
+    protected function write(LogRecord $record): void
     {
         if (!$this->endpoint || !$this->apiKey) return;
 
         Http::withHeaders([
             'Authorization' => 'Bearer ' . $this->apiKey,
         ])->post($this->endpoint, [
-            'message' => $record['message'],
-            'level' => $record['level_name'],
-            'datetime' => $record['datetime']->format('Y-m-d H:i:s'),
-            'context' => $record['context'],
+            'message' => $record->message,
+            'level' => $record->levelName,
+            'datetime' => $record->datetime->format('Y-m-d H:i:s'),
+            'context' => $record->context,
         ]);
     }
 }
